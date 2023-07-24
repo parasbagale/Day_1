894a64c3fc36eed3015db9c7d23683323f255d2b
parasbagale <parasbagale.nc@gmail.com>
2023-07-24 01:30:53 -0400

    commit

---
+++ day2/areaOfRectangle.java
@@ -0,0 +1,14 @@
+package day2;
+
+public class areaOfRectangle {
+    public static void main(String[] args){
+
+        int length = 12;
+        int breadth = 6;
+        int area = length*breadth;
+        System.out.println("Area of rectangle = " + area);
+
+    }
+
+
+}
